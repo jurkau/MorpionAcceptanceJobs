e147145877e7620a4d4ea62ae09d3ad2cf32d83d
Hugo Weber <hugo.weber@edu.univ-fcomte.fr>
2021-03-12 09:40:04 +0100

    commit symbole + plateau

--- src/main/java/morpion/Morpion.java
+++ src/main/java/morpion/Morpion.java
@@ -12,6 +12,50 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
-public class Morpion  {
+public class Morpion  extends Application {
 
+    private static GridPane gameBoard;
+    private static Plateau board;
+    private AnimationTimer gameTimer;
+    private MenuBar menuBar;
+    private Menu gameMenu;
+    private MenuItem newGameOption;
+    private BorderPane root;
+
+    public final static class Tuile extends Button {
+
+        private final int row;
+        private final int col;
+        private Symbole mark;
+
+        public Tuile(int initRow, int initCol, Symbole initMark)
+        {
+            row = initRow;
+            col = initCol;
+            mark = initMark;
+            initialiseTuile();
+        }
+
+        private void initialiseTuile() {
+            this.setOnMouseClicked(e -> {
+                if (!board.isCrossTurn()) {
+                    board.placeSymbole(this.row, this.col);
+                    this.update();
+                }
+            });
+            this.setStyle("-fx-font-style: 70");
+            this.setTextAlignment(TextAlignment.CENTER);
+            this.setMinSize(150.0, 150.0);
+            this.setText("" + this.mark);
+        }
+
+        public void update() {
+            this.mark = board.getMarkAt(this.row, this.col);
+            this.setText("" + mark);
+        }
+    }
+
+    public void start(Stage stage) throws Exception {
+
+    }
 }
